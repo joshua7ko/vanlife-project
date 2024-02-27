bb8c139453f9d42993446a4eec37194f30293d0c
Joshua <joshua7ko@gmail.com>
2024-02-27 20:00:18 +0530

    Added a code correction

--- utils.js
+++ utils.js
@@ -1,8 +1,31 @@
-import { redirect } from "react-router-dom"
+import {redirect} from 'react-router'
 
 export async function requireAuth() {
-    const isLoggedIn = false
-    if (!isLoggedIn) {
-     throw redirect("/login")
-    }
+ const isLoggedIn = false
+
+ if (!isLoggedIn) {
+  const response = redirect('/login')
+  response.body = true
+  throw response
+ }
+ return null
 }
+
+
+
+
+
+
+
+
+
+
+// import { redirect } from "react-router-dom"
+
+// export async function requireAuth() {
+//     const isLoggedIn = false
+//     if (!isLoggedIn) {
+//      return redirect("/login");
+//     }
+    
+// }
